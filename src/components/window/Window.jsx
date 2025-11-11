dc3a8cf4f78adf0ab32beeb7913b63b5e4f6ed87
ChaJiTae <wlxo402@naver.com>
2025-11-12 02:09:02 +0900

    fix : 임시 process Icon 재설정

--- src/components/window/Window.jsx
+++ src/components/window/Window.jsx
@@ -1,9 +1,10 @@
 import TitleBar from "./TitleBar";
+import ProcessIcon from "/src/assets/processicon.jpg";
 
 const Window = () => {
   return (
     <div className="bg-white w-[400px] h-[300px] rounded-xl">
-      <TitleBar icon="/src/assets/processicon.jpg" title="process" />
+      <TitleBar icon={ProcessIcon} title="process" />
     </div>
   );
 };
