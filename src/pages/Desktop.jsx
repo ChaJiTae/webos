84e79c9dc9c4be4750afd1d4abee9ba08108d9ea
ChaJiTae <wlxo402@naver.com>
2025-11-16 01:28:36 +0900

    fix : 배경화면 이미지 파일 크기 수정 및 배경화면 설정

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -1,14 +1,18 @@
 import MenuBar from "../components/layout/MenuBar";
 import Window from "../components/window/Window";
 
+import GrayBackground from "../assets/backgroundImage/Gray_Background.png";
+import DarkBackground from "../assets/backgroundImage/Dark_Background.png";
+import LightBackground from "../assets/backgroundImage/Light_Background.png";
+
 const Desktop = () => {
   return (
     <div className="fixed inset-0 flex flex-col overflow-hidden">
       <MenuBar />
       <div
-        className="relative flex-1"
+        className="relative flex-1 bg-center bg-cover"
         style={{
-          backgroundImage: `linear-gradient(to bottom,#A8E6CF,#6DD5ED)`,
+          backgroundImage: `url(${GrayBackground})`,
         }}
       >
         <Window />
