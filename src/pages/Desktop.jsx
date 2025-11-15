558e909cdd7c799f0fd7f0b795f9c8c9ed726986
ChaJiTae <wlxo402@naver.com>
2025-11-16 01:31:59 +0900

    feat : 랜덤 배경화면 적용

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -5,14 +5,18 @@ import GrayBackground from "../assets/backgroundImage/Gray_Background.png";
 import DarkBackground from "../assets/backgroundImage/Dark_Background.png";
 import LightBackground from "../assets/backgroundImage/Light_Background.png";
 
+const background = [GrayBackground, DarkBackground, LightBackground];
+
 const Desktop = () => {
+  const backgroundRandomIndex = Math.floor(Math.random() * background.length);
+
   return (
     <div className="fixed inset-0 flex flex-col overflow-hidden">
       <MenuBar />
       <div
         className="relative flex-1 bg-center bg-cover"
         style={{
-          backgroundImage: `url(${GrayBackground})`,
+          backgroundImage: `url(${background[backgroundRandomIndex]})`,
         }}
       >
         <Window />
