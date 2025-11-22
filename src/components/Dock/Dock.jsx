3d3b7c9c7cd9d664cf893907219133d8920a1cd7
ChaJiTae <wlxo402@naver.com>
2025-11-22 09:09:32 +0900

    feat : 기본 어플리케이션 docks 넣어두기

--- src/components/Dock/Dock.jsx
+++ src/components/Dock/Dock.jsx
@@ -1,10 +1,30 @@
 import { DockIcon } from "./DockIcon";
 
+import CalculateIcon from "../../assets/processIcon/calculate.png";
+import LottoIcon from "../../assets/processIcon/lotto.png";
+import RacingIcon from "../../assets/processIcon/racing.png";
+import TerminalIcon from "../../assets/processIcon/Terminal.png";
+import NotepadIcon from "../../assets/processIcon/notepad.png";
+
 const Dock = () => {
+  const defaultApplications = [
+    { icon: CalculateIcon, label: "Calculate", processName: "Calculate" },
+    { icon: LottoIcon, label: "Lotto", processName: "Lotto" },
+    { icon: RacingIcon, label: "Racing", processName: "Racing" },
+    { icon: TerminalIcon, label: "Terminal", processName: "Terminal" },
+    { icon: NotepadIcon, label: "Notepad", processName: "Notepad" },
+  ];
+
   return (
     <div className="fixed transform -translate-x-1/2 bottom-5 left-1/2">
       <div className="flex items-end gap-2 px-2 py-2 border shadow-2xl bg-white/10 backdrop-blur-xl rounded-2xl border-white/20">
-        <DockIcon />
+        {defaultApplications.map((application, index) => (
+          <DockIcon
+            key={index}
+            icon={application.icon}
+            label={application.label}
+          />
+        ))}
       </div>
     </div>
   );
