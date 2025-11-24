cb7252e121fec1af291037be86a118451acd04ee
ChaJiTae <wlxo402@naver.com>
2025-11-24 09:25:18 +0900

    fix : Terminal 기본 폰트 변경

--- src/components/Process/Terminal.jsx
+++ src/components/Process/Terminal.jsx
@@ -1,6 +1,6 @@
 const Terminal = () => {
   return (
-    <div className="w-full h-full font-mono text-green-400 bg-black">
+    <div className="w-full h-full text-green-400 bg-black">
       <p>$ Welcome to Terminal</p>
       <h1 className="flex items-center justify-center h-full text-2xl">
         아직 개발중입니다.
